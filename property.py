260ba4769841983df5c80707a1b4c2862041cd8f
Srinath-N-R <htnaris@gmail.com>
2022-07-06 08:50:19 -0700

    Update property.py

--- property.py
+++ property.py
@@ -122,7 +122,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
 callbacks.append(cp_callback)
 
 history = model.fit(
-    train_ds_batched, epochs=2, validation_data=val_ds_batched, callbacks=[callbacks]
+    train_ds_batched, epochs=100, validation_data=val_ds_batched, callbacks=[callbacks]
 )
 
 iou = history.history["mean_iou"]
