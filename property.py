4cf60d84308ab39804244cd54028b809ff4ade29
Srinath-N-R <htnaris@gmail.com>
2022-07-08 14:16:57 -0700

    Update property.py

--- property.py
+++ property.py
@@ -30,7 +30,6 @@ parser.add_argument("--monitor", type=str, default="val_loss")
 parser.add_argument("--lr", type=float, default=0.005)
 parser.add_argument("--batch_size", type=int, default=32)
 parser.add_argument("--patience", type=int, default=20)
-parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--save_path", type=str, default="property")
 
 args, _ = parser.parse_known_args()
@@ -65,32 +64,33 @@ val_label_names = [
     dataset_directory + "/val_labels/" + i for i in os.listdir(dataset_directory + "/val/") if i.endswith(".png")
 ]
 
-x_train = []
-y_train = []
-for i in range(len(train_input_names)):
-    img = cv2.imread(train_input_names[i])
-    mask = cv2.imread(train_label_names[i])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-    mask = mask.astype(np.float32)
-    x_train.append(img)
-    y_train.append(mask)
+# x_train = []
+# y_train = []
+# for i in range(len(train_input_names)):
+#     img = cv2.imread(train_input_names[i])
+#     mask = cv2.imread(train_label_names[i])
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     img = img.reshape(1,256,256,3)
+#     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+#     mask = img.reshape(1, 256, 256, 3)
+#     x_train.append(img)
+#     y_train.append(mask)
+#
+# x_val = []
+# y_val = []
+# for i in range(len(val_input_names)):
+#     img = cv2.imread(val_input_names[i])
+#     mask = cv2.imread(val_label_names[i])
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     img = img.reshape(1,256,256,3)
+#     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+#     x_val.append(img)
+#     y_val.append(mask)
+#
+# train_ds = tf.data.Dataset.from_tensor_slices((np.array(x_train), np.array(y_train)))
+# val_ds = tf.data.Dataset.from_tensor_slices((np.array(x_train), np.array(y_train)))
 
-x_val = []
-y_val = []
-for i in range(len(val_input_names)):
-    img = cv2.imread(val_input_names[i])
-    mask = cv2.imread(val_label_names[i])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-    mask = mask.astype(np.float32)
-    x_val.append(img)
-    y_val.append(mask)
-
-train_ds = tf.data.Dataset.from_tensor_slices((np.array(x_train), np.array(y_train))).batch(16, drop_remainder=True)
-val_ds = tf.data.Dataset.from_tensor_slices((np.array(x_val), np.array(y_val))).batch(16, drop_remainder=True)
-
-# train_ds_batched, val_ds_batched = create_dataset(train_input_names, val_input_names, train_augmentation=args_dict["train_augmentation_file"])
+train_ds_batched, val_ds_batched = create_dataset(train_input_names, val_input_names, train_augmentation=args_dict["train_augmentation_file"])
 
 builder = SM_UNet_Builder(
     encoder_name='efficientnetv2-l',
@@ -103,8 +103,33 @@ builder = SM_UNet_Builder(
     head_dropout=0,  # dropout at head
     dropout=0,  # dropout at feature extraction
 )
+
+def dice_per_class(y_true, y_pred, eps=1e-5):
+    intersect = tf.reduce_sum(y_true * y_pred)
+    y_sum = tf.reduce_sum(y_true * y_true)
+    z_sum = tf.reduce_sum(y_pred * y_pred)
+    loss = 1 - (2 * intersect + eps) / (z_sum + y_sum + eps)
+    return loss
+
+def gen_dice(y_true, y_pred):
+    """both tensors are [b, h, w, classes] and y_pred is in logit form"""
+    # [b, h, w, classes]
+    pred_tensor = tf.nn.softmax(y_pred)
+    loss = 0.0
+    for c in range(3):
+        loss += dice_per_class(y_true[:, :, :, c], pred_tensor[:, :, :, c])
+    return loss / 3
+
+def segmentation_loss(y_true, y_pred):
+    cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    cross_entropy_loss = cce(y_true=y_true, y_pred=y_pred)
+    dice_loss = gen_dice(y_true, y_pred)
+    return 0.5 * cross_entropy_loss + 0.5 * dice_loss
+
+
+
 model = builder.build_model()
-model.compile(optimizer='adam', loss=BinaryFocalLoss(gamma=2), metrics=mean_iou)
+model.compile(optimizer='adam', loss=segmentation_loss, metrics=mean_iou)
 
 env = Environment()
 
@@ -141,7 +166,7 @@ callbacks.append(cp_callback)
 
 
 history = model.fit(
-    train_ds, epochs=100, validation_data=val_ds, callbacks=[callbacks]
+    train_ds_batched, epochs=100, validation_data=val_ds_batched, callbacks=[callbacks]
 )
 
 iou = history.history["mean_iou"]
@@ -152,8 +177,6 @@ val_loss = history.history["val_loss"]
 df = pd.DataFrame(loss)
 df["loss"] = loss
 df["val_loss"] = val_loss
-df["mean_iou"] = iou
-df["val_mean_iou"] = val_iou
 
 df.to_csv("parcelUnet.csv")
 
