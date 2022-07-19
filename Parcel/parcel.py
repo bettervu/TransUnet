1a56f927f5e5d9146d6fd67bdd61748733bc1397
Srinath-N-R <htnaris@gmail.com>
2022-07-19 13:11:42 -0700

    Update parcel.py

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications import ResNet152V2
 from tensorflow.keras.layers import Input,Conv2D,Dropout,Reshape
 
@@ -42,10 +41,10 @@ X = [i/255.0 for i in X]
 X = np.array(X)
 y = np.array(df["coords_vals"].to_list())
 
-x_train = np.asarray(X[:-100])
-y_train = np.asarray(y[:-100])
-x_val = np.asarray(X[-100:])
-y_val = np.asarray(y[-100:])
+x_train = X[:-100]
+y_train = y[:-100]
+x_val = X[-100:]
+y_val = y[-100:]
 
 model = Sequential([
     Input(shape=(256,256,3)),
@@ -63,22 +62,8 @@ model.compile(optimizer='adam',
              loss='mse',
              metrics=['accuracy'])
 
-callbacks = []
 
-early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=24)
-
-callbacks.append(early_stopping)
-
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath="CKPT/checkpoint/",
-            save_weights_only=True,
-            monitor='val_loss',
-            mode='min',
-            save_best_only=True)
-
-callbacks.append(cp_callback)
-
-H = model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=16, epochs=100,verbose=1, callbacks=[callbacks])
+H = model.fit(x_train,y_train, validation_data=(x_val,y_val), batch_size=16, epochs=100,verbose=1)
 
 loss = H.history["loss"]
 val_loss = H.history["val_loss"]
