2de181c17f6c38b9f6657cb59b9dc73ac51ea72a
Srinath-N-R <htnaris@gmail.com>
2022-07-21 15:05:15 -0700

    Update parcel.py

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -1,19 +1,19 @@
+import math
+import operator
 import os
+import tarfile
+from functools import reduce
+from random import sample
+
 import cv2
 import gcsfs
-import tarfile
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from random import sample
 from tensorflow.keras import Sequential
-from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications import ResNet152V2
-from tensorflow.keras.layers import Input,Conv2D,Dropout,Reshape
-
-from functools import reduce
-import operator
-import math
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Conv2D, Dropout, Input, Reshape
 
 FS = gcsfs.GCSFileSystem()
 
@@ -23,21 +23,23 @@ try:
 except:
     print("Gpus not found")
 
+
 def extend_list(lol):
     if len(lol) >= 10:
         lol = sample(lol, 10)
     else:
-        lol.extend((10-len(lol))*[[0,0]])
+        lol.extend((10 - len(lol)) * [[0, 0]])
     lol = np.array([(np.array(i).flatten()) for i in lol]).flatten()
     return lol
+
 
 def flatten(lol):
     lol = np.array([(np.array(i).flatten()) for i in lol]).flatten()
     return lol
 
 
-df=pd.read_csv("dataset.csv")
-df["coords_vals"]=df["coords_vals"].apply(eval)
+df = pd.read_csv("dataset.csv")
+df["coords_vals"] = df["coords_vals"].apply(eval)
 
 images = []
 for i in range(4665):
@@ -45,33 +47,36 @@ for i in range(4665):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     images.append(img)
 
-def distance(l1, l2=[0,0]):
-    d = ((l1[0] - l2[0])**2 + (l1[1] - l2[1])**2)**0.5
+
+def distance(l1, l2=[0, 0]):
+    d = ((l1[0] - l2[0]) ** 2 + (l1[1] - l2[1]) ** 2) ** 0.5
     return d
+
 
 def sort_coords(coords):
     center = tuple(map(operator.truediv, reduce(lambda x, y: map(operator.add, x, y), coords), [len(coords)] * 2))
     coords = (sorted(coords, key=lambda coord: (-135 - math.degrees(math.atan2(*tuple(map(operator.sub, coord, center))[::-1]))) % 360))
-    dst = list(map(distance,coords))
+    dst = list(map(distance, coords))
     origin = dst.index(min(dst))
-    final_coords = coords[origin:]+coords[:origin]
+    final_coords = coords[origin:] + coords[:origin]
     return final_coords
 
+
 df["images"] = images
-df = df[df["after_cleanup_len"]<=10]
+df = df[df["after_cleanup_len"] == 10]
 df["sorted_coords"] = df["coords_vals"].apply(sort_coords)
-df["sorted_coords"]=df["sorted_coords"].apply(extend_list)
+df["sorted_coords"] = df["sorted_coords"].apply(extend_list)
 
 # df["coords_vals"]=df["coords_vals"].apply(extend_list)
 
 X = df["images"].to_list()
-X = [i/255.0 for i in X]
+X = [i / 255.0 for i in X]
 X = np.array(X)
 y = np.array(df["sorted_coords"].to_list())
 
 model = Sequential([
-    Input(shape=(256,256,3)),
-    ResNet152V2(include_top=False, input_shape=(256,256,3)),
+    Input(shape=(256, 256, 3)),
+    ResNet152V2(include_top=False, input_shape=(256, 256, 3)),
     Conv2D(512, 3, padding='same', activation='relu'),
     Conv2D(512, 3, padding='same', activation='relu'),
     Conv2D(256, 3, 2, padding='same', activation='relu'),
@@ -91,7 +96,7 @@ early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patien
 
 callbacks.append(early_stopping)
 
-H = model.fit(np.asarray(X[:-50]), np.asarray(y[:-50]), validation_data=(X[50:], y[50:]), batch_size=16, epochs=100,verbose=1)
+H = model.fit(np.asarray(X[:-50]), np.asarray(y[:-50]), validation_data=(X[50:], y[50:]), batch_size=16, epochs=100, verbose=1)
 
 loss = H.history["loss"]
 val_loss = H.history["val_loss"]
