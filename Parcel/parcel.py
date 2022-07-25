31f28862f6b56c25c383d9851cb6633bc1cbc1da
Srinath-N-R <htnaris@gmail.com>
2022-07-25 12:03:42 -0700

    Update parcel.py

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -4,6 +4,7 @@ import os
 import tarfile
 from functools import reduce
 from random import sample
+
 import cv2
 import gcsfs
 import numpy as np
@@ -12,7 +13,9 @@ import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.applications import ResNet152V2
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Conv2D, Dropout, Input, Reshape, Permute, Flatten, Dense
+from tensorflow.keras.layers import (Conv2D, Dense, Dropout, Flatten, Input,
+                                     Permute, Reshape)
+
 FS = gcsfs.GCSFileSystem()
 try:
     gpus = tf.config.list_physical_devices("GPU")
@@ -29,7 +32,9 @@ def extend_list(lol):
     lol = np.array([(np.array(i).flatten()) for i in lol]).flatten()
     return lol
 
+
 n_coords = 7
+
 
 def interpolate(lol, n=n_coords, t="same"):
     if len(lol) == n:
@@ -60,43 +65,50 @@ def interpolate(lol, n=n_coords, t="same"):
         lol = np.array([np.array([final_x[pt], final_y[pt]]) for pt in range(len(final_x))])
     return lol
 
+
 def flatten(lol):
     lol = np.array([(np.array(i).flatten()) for i in lol]).flatten()
     return lol
+
 
 def bbox(lol):
     x = [pt[0] for pt in lol]
     y = [pt[1] for pt in lol]
     return np.array([min(x), min(y), max(x), max(y)])
 
-df = pd.read_csv("dataset.csv")
-df["coords_vals"] = df["coords_vals"].apply(eval)
 
-images = []
-missing = []
-for i in df.index:
-    try:
-        img = cv2.imread(f"test_parcel/train/{df['gtu_ids'][i]}.png")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        images.append(img)
-        if img.shape[2] == 4:
-            missing.append(i)
-    except:
-        missing.append(i)
-df.drop(missing, inplace=True)
+def load_image(x):
+    img = cv2.imread(x)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+
 def distance(l1, l2=[0, 0]):
     d = ((l1[0] - l2[0]) ** 2 + (l1[1] - l2[1]) ** 2) ** 0.5
     return d
+
+
 def sort_coords(coords):
     center = tuple(map(operator.truediv, reduce(lambda x, y: map(operator.add, x, y), coords), [len(coords)] * 2))
-    coords = (sorted(coords, key=lambda coord: (-135 - math.degrees(math.atan2(*tuple(map(operator.sub, coord, center))[::-1]))) % 360))
+    coords = sorted(
+        coords,
+        key=lambda coord: (-135 - math.degrees(math.atan2(*tuple(map(operator.sub, coord, center))[::-1]))) % 360,
+    )
     dst = list(map(distance, coords))
     origin = dst.index(min(dst))
     final_coords = coords[origin:] + coords[:origin]
     return final_coords
 
 
-df["images"] = images
+df = pd.read_csv("dataset.csv")
+df["coords_vals"] = df["coords_vals"].apply(eval)
+
+files = os.listdir("test_parcel/train")
+files = [eval(file.split(".")[0]) for file in files]
+
+allowable_train_gtus = list(set(files).intersection(set(df["gtu_ids"])))
+df = df[df["gtu_ids"].isin(allowable_train_gtus)]
+
 df = df[(df["after_cleanup_len"] <= n_coords)]
 df["sorted_coords"] = df["coords_vals"].apply(sort_coords)
 df["interpolate"] = df["sorted_coords"].apply(interpolate)
@@ -104,34 +116,61 @@ df["interpolate"] = df["interpolate"].apply(sort_coords)
 df["interpolate"] = df["interpolate"].apply(flatten)
 df["bbox"] = df["sorted_coords"].apply(bbox)
 
+train_df = df.sample(frac=0.8)
+val_df = df.drop(train_df.index)
+train_images = tf.data.Dataset.from_tensor_slices(
+    [f"test_parcel/train/{train_df['gtu_ids'][i]}.png" for i in train_df.index]
+)
+train_images = train_images.map(load_image)
+train_images = train_images.map(lambda x: tf.ensure_shape(x, [256, 256, 3]))
+val_images = tf.data.Dataset.from_tensor_slices([f"test_parcel/train/{val_df['gtu_ids'][i]}.png" for i in val_df.index])
+val_images = val_images.map(load_image)
+val_images = val_images.map(lambda x: tf.ensure_shape(x, [256, 256, 3]))
 
-X = df["images"].to_list()
-# X = [i / 255.0 for i in X]
-X = np.array(X)
-y = np.array(df["interpolate"].to_list())
-model = Sequential([
-    Input(shape=(512,512,3)),
-    ResNet152V2(include_top=False, input_shape=(512,512,3)),
-    Conv2D(1024, 3, padding='same', activation='relu'),
-    Conv2D(512, 3, padding='same', activation='relu'),
-    Conv2D(256, 3, 2, padding='same', activation='relu'),
-    Conv2D(256, 2, 2, activation='relu'),
-    Dropout(0.05),
-    Conv2D(2*n_coords, 2, 2),
-    Permute((3,2,1), input_shape=(2, 2, 2*n_coords)),
-    Flatten(),
-    Dense(4*n_coords, activation="relu"),
-    Dense(2*n_coords, activation="relu"),])
+
+y_train = np.array(train_df["interpolate"].to_list())
+y_val = np.array(val_df["interpolate"].to_list())
+
+train_labels = tf.data.Dataset.from_tensor_slices(y_train)
+val_labels = tf.data.Dataset.from_tensor_slices(y_val)
+
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(5000)
+train = train.batch(16)
+train = train.prefetch(4)
+
+val = tf.data.Dataset.zip((val_images, val_labels))
+val = val.shuffle(1000)
+val = val.batch(16)
+val = val.prefetch(4)
+
+
+model = Sequential(
+    [
+        Input(shape=(512, 512, 3)),
+        ResNet152V2(include_top=False, input_shape=(512, 512, 3)),
+        Conv2D(1024, 3, padding="same", activation="relu"),
+        Conv2D(512, 3, padding="same", activation="relu"),
+        Conv2D(256, 3, 2, padding="same", activation="relu"),
+        Conv2D(256, 2, 2, activation="relu"),
+        Dropout(0.05),
+        Conv2D(2 * n_coords, 2, 2),
+        Permute((3, 2, 1), input_shape=(2, 2, 2 * n_coords)),
+        Flatten(),
+        Dense(4 * n_coords, activation="relu"),
+        Dense(2 * n_coords, activation="relu"),
+    ]
+)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, decay=0.0007)
 loss = tf.keras.losses.MeanSquaredError()
 model.compile(optimizer, loss)
 callbacks = []
-early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
+early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=20)
 
 callbacks.append(early_stopping)
 
-H = model.fit(np.asarray(X[:-300]), np.asarray(y[:-300]), validation_data=(X[300:], y[300:]), batch_size=12, epochs=100, verbose=1, callbacks=callbacks)
+H = model.fit(train, validation_data=val, batch_size=12, epochs=100, verbose=1, callbacks=callbacks)
 
 loss = H.history["loss"]
 val_loss = H.history["val_loss"]
