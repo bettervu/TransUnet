c23fb70373a782ab63544df805d199ca5bf0b821
Srinath-N-R <htnaris@gmail.com>
2022-07-26 15:20:54 -0700

    c

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -182,7 +182,7 @@ H = model.fit(
     np.asarray(y[:-125]),
     validation_data=(X[-125:], y[-125:]),
     batch_size=16,
-    epochs=50,
+    epochs=20,
     verbose=1,
     callbacks=callbacks,
 )
