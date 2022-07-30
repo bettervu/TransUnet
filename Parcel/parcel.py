3790d742a35e044329bbaa2d3b47838c5ba39d9f
Srinath-N-R <htnaris@gmail.com>
2022-07-30 13:43:16 -0700

    Update parcel.py

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -376,7 +376,7 @@ H = model.fit(
     np.asarray(y[:-125]),
     validation_data=(X[-125:], y[-125:]),
     batch_size=16,
-    epochs=20,
+    epochs=5,
     verbose=1,
     callbacks=callbacks,
 )
