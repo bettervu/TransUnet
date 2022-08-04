67510f47c84a451ae2355ba1069bb6233f129d07
Srinath-N-R <htnaris@gmail.com>
2022-08-04 07:53:38 -0700

    Update parcel.py

--- Parcel/parcel.py
+++ Parcel/parcel.py
@@ -40,78 +40,82 @@ df["center"] = df["sorted_coords"].apply(center)
 
 df["new"] = df.apply(lambda x: np.concatenate((x["bbox"], x["center"], x["edges"])), axis=1)
 
-files = os.listdir("test_parcel/train")
-try:
-    files.remove(".DS_Store")
-except:
-    print("No hidden file encountered")
-files = [int(file.split(".")[0]) for file in files]
-allowable_train_gtus = list(set(files).intersection(set(df["gtu_ids"])))
-df = df[df["gtu_ids"].isin(allowable_train_gtus)]
-images = []
-missing = []
-for i in df.index:
-    try:
-        img = cv2.imread(f"test_parcel/train/{df['gtu_ids'][i]}.png")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        images.append(img)
-        if img.shape[2] == 4:
-            missing.append(i)
-    except:
-        missing.append(i)
-df.drop(missing, inplace=True)
-df["images"] = images
-print("No error until now")
+df["new"] = df["new"].apply(lambda x:[list(i) for i in x])
 
-X = df["images"].to_list()
-X = np.array(X)
-y = np.array(df["new"].to_list())
-# y = np.array(df["bbox"].to_list())
+df.to_csv("dataset.csv")
 
-builder = SM_UNet_Builder(
-    encoder_name="efficientnetv2-l",
-    input_shape=(256, 256, 3),
-    num_classes=3,
-    activation="softmax",
-    train_encoder=False,
-    encoder_weights="imagenet",
-    decoder_block_type="upsampling",
-    head_dropout=0,  # dropout at head
-    dropout=0,
-)
-model1 = builder.build_model()
-model = Sequential(
-    [
-        Input(shape=(256, 256, 3)),
-        model1,
-        Conv2D((2 * n_coords) + 6 + 2, 2, 2),
-        Flatten(),
-        Dense(((2 * n_coords) + 6 + 2), activation="relu"),
-    ]
-)
-
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, decay=0.0007)
-loss = tf.keras.losses.MeanSquaredError()
-model.compile(optimizer, loss)
-callbacks = []
-early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=20)
-callbacks.append(early_stopping)
-H = model.fit(
-    np.asarray(X[:-500]),
-    np.asarray(y[:-500]),
-    validation_data=(X[-500:], y[-500:]),
-    batch_size=8,
-    epochs=4,
-    verbose=1,
-    callbacks=callbacks,
-)
-loss = H.history["loss"]
-val_loss = H.history["val_loss"]
-df = pd.DataFrame(loss)
-df["loss"] = loss
-df["val_loss"] = val_loss
-df.to_csv("parcelUnet.csv")
-model.save("my_model")
-with tarfile.open("my_model.tar.gz", "w:gz") as tar:
-    tar.add("my_model", arcname=os.path.basename("my_model"))
+# files = os.listdir("test_parcel/train")
+# try:
+#     files.remove(".DS_Store")
+# except:
+#     print("No hidden file encountered")
+# files = [int(file.split(".")[0]) for file in files]
+# allowable_train_gtus = list(set(files).intersection(set(df["gtu_ids"])))
+# df = df[df["gtu_ids"].isin(allowable_train_gtus)]
+# images = []
+# missing = []
+# for i in df.index:
+#     try:
+#         img = cv2.imread(f"test_parcel/train/{df['gtu_ids'][i]}.png")
+#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         images.append(img)
+#         if img.shape[2] == 4:
+#             missing.append(i)
+#     except:
+#         missing.append(i)
+# df.drop(missing, inplace=True)
+# df["images"] = images
+# print("No error until now")
+#
+# X = df["images"].to_list()
+# X = np.array(X)
+# y = np.array(df["new"].to_list())
+# # y = np.array(df["bbox"].to_list())
+#
+# builder = SM_UNet_Builder(
+#     encoder_name="efficientnetv2-l",
+#     input_shape=(256, 256, 3),
+#     num_classes=3,
+#     activation="softmax",
+#     train_encoder=False,
+#     encoder_weights="imagenet",
+#     decoder_block_type="upsampling",
+#     head_dropout=0,  # dropout at head
+#     dropout=0,
+# )
+# model1 = builder.build_model()
+# model = Sequential(
+#     [
+#         Input(shape=(256, 256, 3)),
+#         model1,
+#         Conv2D((2 * n_coords) + 6 + 2, 2, 2),
+#         Flatten(),
+#         Dense(((2 * n_coords) + 6 + 2), activation="relu"),
+#     ]
+# )
+#
+#
+# optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, decay=0.0007)
+# loss = tf.keras.losses.MeanSquaredError()
+# model.compile(optimizer, loss)
+# callbacks = []
+# early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=20)
+# callbacks.append(early_stopping)
+# H = model.fit(
+#     np.asarray(X[:-500]),
+#     np.asarray(y[:-500]),
+#     validation_data=(X[-500:], y[-500:]),
+#     batch_size=8,
+#     epochs=4,
+#     verbose=1,
+#     callbacks=callbacks,
+# )
+# loss = H.history["loss"]
+# val_loss = H.history["val_loss"]
+# df = pd.DataFrame(loss)
+# df["loss"] = loss
+# df["val_loss"] = val_loss
+# df.to_csv("parcelUnet.csv")
+# model.save("my_model")
+# with tarfile.open("my_model.tar.gz", "w:gz") as tar:
+#     tar.add("my_model", arcname=os.path.basename("my_model"))
