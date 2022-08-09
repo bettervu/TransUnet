b48bb38550cd2bcb0d457fe237514fc1ffc485a4
Srinath-N-R <htnaris@gmail.com>
2022-08-09 12:39:07 -0700

    Update parcelUnet.py

--- Parcel/parcelUnet.py
+++ Parcel/parcelUnet.py
@@ -39,27 +39,23 @@ df["gtu_ids"] = allowable_gtus
 train_df = df.sample(frac=0.8)
 val_df = df.drop(train_df.index)
 
-train_images = tf.data.Dataset.from_tensor_slices(
-    [f"test_parcel/train/{train_df['gtu_ids'][i]}.png" for i in train_df.index]
-)
+train_images = [f"test_parcel/train/{train_df['gtu_ids'][i]}.png" for i in train_df.index]
 # train_images = train_images.map(load_image)
 
-train_labels = tf.data.Dataset.from_tensor_slices(
-    [f"test_parcel/train_labels/{train_df['gtu_ids'][i]}.png" for i in train_df.index]
-)
+train_labels = [f"test_parcel/train_labels/{train_df['gtu_ids'][i]}.png" for i in train_df.index]
 # train_labels = train_labels.map(load_image)
 
-val_images = tf.data.Dataset.from_tensor_slices([f"test_parcel/train/{val_df['gtu_ids'][i]}.png" for i in val_df.index])
+val_images = [f"test_parcel/train/{val_df['gtu_ids'][i]}.png" for i in val_df.index]
 # val_images = val_images.map(load_image)
 
-val_labels = tf.data.Dataset.from_tensor_slices([f"test_parcel/train_labels/{val_df['gtu_ids'][i]}.png" for i in val_df.index])
+val_labels = [f"test_parcel/train_labels/{val_df['gtu_ids'][i]}.png" for i in val_df.index]
 # val_labels = val_labels.map(load_image)
 
 train, val = create_dataset(train_images, val_images, train_augmentation="dTurk/dTurk/augmentation/configs/light.yaml")
 
 
 builder = SM_UNet_Builder(
-    encoder_name='efficientnetv2-l',
+    encoder_name="efficientnetv2-l",
     input_shape=(256, 256, 3),
     num_classes=3,
     activation="softmax",
@@ -70,13 +66,11 @@ builder = SM_UNet_Builder(
     dropout=0,
 )
 
-loss = DiceLoss(class_weights=[1,1,1], class_indexes=[0,1,2], per_image=False)
-metric = WeightedMeanIoU(
-            num_classes=3, class_weights=[1,1,1], name="wt_mean_iou"
-        )
+loss = DiceLoss(class_weights=[1, 1, 1], class_indexes=[0, 1, 2], per_image=False)
+metric = WeightedMeanIoU(num_classes=3, class_weights=[1, 1, 1], name="wt_mean_iou")
 
 model = builder.build_model()
-model.compile(optimizer='adam', loss=loss, metrics=metric)
+model.compile(optimizer="adam", loss=loss, metrics=metric)
 
 callbacks = []
 early_stopping = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=4)
@@ -103,5 +97,3 @@ df.to_csv("parcelUnet.csv")
 model.save("my_model_Unet")
 with tarfile.open("my_model_Unet.tar.gz", "w:gz") as tar:
     tar.add("my_model_Unet", arcname=os.path.basename("my_model_Unet"))
-
-
