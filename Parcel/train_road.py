f029305aabb919dd3692f7c8a5000106985bddb2
Srinath-N-R <htnaris@gmail.com>
2022-08-24 15:24:02 -0700

    Update train_road.py

--- Parcel/train_road.py
+++ Parcel/train_road.py
@@ -14,7 +14,7 @@ from dTurk.metrics import WeightedMeanIoU
 FS = gcsfs.GCSFileSystem()
 try:
     gpus = tf.config.list_physical_devices("GPU")
-    tf.config.set_visible_devices(gpus[2], "GPU")
+    tf.config.set_visible_devices(gpus[0], "GPU")
 except:
     print("Gpus not found")
 
